20ff54f3cc0df15ad438ee8be125a3ee95a227a8
Stefan van der Ende <scende@avans.nl>
2016-04-20 14:31:04 +0200

    changed things

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -8,6 +8,8 @@ public class MainClass {
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		
+		
+		System.out.println("‚r‚l‚`‚k‚k@‚k‚n‚`‚m@‚n‚e@‚`@‚l‚h‚k‚k‚h‚n‚m@‚c‚n‚k‚k‚`‚q‚r");
 		Game boot = new Game();
 		
 	}
